5f998234a2ec494e89b82f889f3be6de727856c0
Juan P. Daza P <juan.daza@er-technology.net>
2014-07-10 20:07:19 -0500

    Opman files added

---
+++ app/src/main/java/net/ertechnology/opman/DisplayMessageActivity.java
@@ -0,0 +1,65 @@
+package net.ertechnology.opman;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.ertechnology.opman.R;
+
+import static net.ertechnology.opman.R.id.container;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+
+        /*if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container,
+                    new PlaceholderFragment()).commit();
+        }*/
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_display_message,
+                    container, false);
+            return rootView;
+        }
+    }
+
+
+}
